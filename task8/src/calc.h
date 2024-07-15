36e89d6a59df0a43361600c6bae0ac858b2b5760
nxk1t <samfishernik@gmail.com>
2024-07-15 10:39:09 -0700

    Dynamic calculator is done

---
+++ task8/src/calc.h
@@ -0,0 +1,13 @@
+#ifndef __CALC_H__
+#define __CALC_H__
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long int add();
+long long int sub();
+long long int mul();
+long long int division();
+
+#endif
