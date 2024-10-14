9d82b4bf0ca8a2dc9137c72a222c21c5061b6062
nxk1t <samfishernik@gmail.com>
2024-10-14 07:13:56 -0700

    added pipe and dup2 to file manager

--- task10/thirdTask/src/main.c
+++ task10/thirdTask/src/main.c
@@ -6,15 +6,20 @@ int main()
     int status;
     char* command;
     char** splitCommand;
+    char** splitCommandSecond;
     char commandPath[STRSIZE];
+    int pipefd[2];
 
     system("clear");
 
     command = (char*)malloc(STRSIZE * sizeof(char));
-    
+
     splitCommand = (char**)malloc(STRSIZE * sizeof(char*));
-    for (int i = 0; i < STRSIZE; i++)
+    splitCommandSecond = (char**)malloc(STRSIZE * sizeof(char*));
+    for (int i = 0; i < STRSIZE; i++) {
         splitCommand[i] = (char*)malloc(STRSIZE * sizeof(char));
+        splitCommandSecond[i] = (char*)malloc(STRSIZE * sizeof(char));
+    }
 
     while (1)
     {
@@ -23,44 +28,129 @@ int main()
         printf("input: ");
         enterCommandString(command);
 
-        splitCommandString(command, splitCommand);
-        strcat(commandPath, splitCommand[0]);
-
         if (splitCommand == NULL)
         {
             perror("Ошибка выделения памяти для splitCommand");
             exit(EXIT_FAILURE);
         }
 
-
-        if (strcmp(command, "exit") == 0)
-            break;
-        else
+        // Проверка на команду cd
+        if (strncmp(command, "cd", 2) == 0)
         {
-            childProcess = fork();
-
-            if (childProcess == 0)
+            splitCommandString(command, splitCommand);
+            if (splitCommand[1] != NULL)
             {
-                execv(commandPath, splitCommand);
-                
-                exit(10);
-            }
-            else if (childProcess < 0)
-            {
-                perror("Произошла ошибка при вызове fork()");
-                exit(EXIT_FAILURE);
+                if (chdir(splitCommand[1]) != 0) {
+                    perror("Ошибка при выполнении команды cd");
+                }
             }
             else
             {
+                fprintf(stderr, "Ошибка: не указан путь для cd\n");
+            }
+        }
+        else if (strcmp(command, "exit") == 0)
+        {
+            break;
+        }
+        else
+        {
+            // Проверяем, есть ли в команде пайп
+            char* pipePosition = strchr(command, '|');
+            if (pipePosition != NULL)
+            {
+                // Разделяем команды по пайпу
+                char* firstCommand = strtok(command, "|");
+                char* secondCommand = strtok(NULL, "|");
+
+                // Убираем лишние пробелы
+                while (*secondCommand == ' ') secondCommand++;
+
+                // Создаем пайп
+                if (pipe(pipefd) == -1)
+                {
+                    perror("Ошибка создания пайпа");
+                    exit(EXIT_FAILURE);
+                }
+
+                // Создаем дочерний процесс для первой команды
+                if (fork() == 0)
+                {
+                    // Закрываем поток чтения
+                    close(pipefd[0]);
+
+                    // Перенаправляем стандартный вывод в пайп
+                    dup2(pipefd[1], STDOUT_FILENO);
+                    close(pipefd[1]);
+
+                    // Выполняем первую команду
+                    splitCommandString(firstCommand, splitCommand);
+                    strcpy(commandPath, "/bin/");
+                    strcat(commandPath, splitCommand[0]);
+                    execv(commandPath, splitCommand);
+                    perror("Ошибка выполнения первой команды");
+                    exit(EXIT_FAILURE);
+                }
+
+                // Создаем дочерний процесс для второй команды
+                if (fork() == 0)
+                {
+                    // Закрываем поток записи
+                    close(pipefd[1]);
+
+                    // Перенаправляем стандартный ввод из пайпа
+                    dup2(pipefd[0], STDIN_FILENO);
+                    close(pipefd[0]);
+
+                    // Выполняем вторую команду
+                    splitCommandString(secondCommand, splitCommandSecond);
+                    strcpy(commandPath, "/bin/");
+                    strcat(commandPath, splitCommandSecond[0]);
+                    execv(commandPath, splitCommandSecond);
+                    perror("Ошибка выполнения второй команды");
+                    exit(EXIT_FAILURE);
+                }
+
+                // Закрываем оба конца пайпа в родительском процессе
+                close(pipefd[0]);
+                close(pipefd[1]);
+
+                // Ждем завершения обоих дочерних процессов
+                wait(&status);
                 wait(&status);
             }
-            clearCommandString(command, splitCommand);
+            else
+            {
+                splitCommandString(command, splitCommand);
+                childProcess = fork();
+
+                if (childProcess == 0)
+                {
+                    strcat(commandPath, splitCommand[0]);
+                    execv(commandPath, splitCommand);
+
+                    perror("Ошибка выполнения команды");
+                    exit(EXIT_FAILURE);
+                }
+                else if (childProcess < 0)
+                {
+                    perror("Произошла ошибка при вызове fork()");
+                    exit(EXIT_FAILURE);
+                }
+                else
+                {
+                    wait(&status);
+                }
+            }
         }
 
+        clearCommandString(command, splitCommand);
+        clearCommandString(command, splitCommandSecond);
     }
 
     free(command);
     free(splitCommand);
+    free(splitCommandSecond);
 
     exit(EXIT_SUCCESS);
 }
